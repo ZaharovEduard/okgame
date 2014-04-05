84261492493061c7b695235c25e25870c2e0811e
ZaharovEduard <eddyzaharov@gmail.com>
2014-04-05 21:51:53 +0400

    ok launcher added

--- launcher.py
+++ launcher.py
@@ -5,47 +5,49 @@ import client
 DEFAULT_PORT = 9043
 MAX_PLAYERS = 20
 
-def run_serv(server, started):
-    if not started:
-        started = True   
-        server.start()
+class App:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry = ('320x320')
+        self.window.resizable(0,0)
+        self.game_name = tk.Label(self.window, text="Super launcher 9000").grid(row=0,column=0)
+        self.addr_label = tk.Label(self.window, text='Ip:').grid(row=1,column=0)
+        self.addr_entr = tk.Entry(self.window,width=21)
+        self.addr_entr.grid(row=1,column=1)
+        self.name_label = tk.Label(self.window, text='Name:').grid(row=2,column=0)
+        self.name_entr = tk.Entry(self.window,width=21)
+        self.name_entr.grid(row=2,column=1)
+        self.pass_label = tk.Label(self.window, text='Password:').grid(row=3,column=0)
+        self.password_entr = tk.Entry(self.window,width=21)
+        self.password_entr.grid(row=3,column=1)
+        self.join_button = tk.Button(self.window, text='Join', command = self.join_game)
+        self.join_button.grid(row=4,column=0)
+        self.start_button = tk.Button(self.window, text='Run server', command = self.run_server)
+        self.start_button.grid(row=4,column=1)
+    
+    def join_game(self):
+        addres = self.addr_entr.get()
+        name = self.name_entr.get()
+        password = self.password_entr.get()
+        client.game(addres, DEFAULT_PORT, name, password)
 
-def join_game(addr, name, password):
-    print(addr,name,password)
-    if isinstance(addr, str) and isinstance(name, str) and isinstance(password, str):
-        client.game(addr, DEFAULT_PORT, name, password)
+    def run_server(self):
+        self.server = server.Server(DEFAULT_PORT, MAX_PLAYERS)
+        self.server.start()
+        print(self.start_button)
+        self.start_button.config(state=tk.DISABLED)
+        print(self.server)
+        if self.server:
+            print(self.server.running)
+    
+    def close_launcher(self):
+        self.server.running = False
+        self.window.destroy()
 
-serv = server.Server(DEFAULT_PORT, MAX_PLAYERS)
+    def start(self):
+        self.window.protocol('WM_DELETE_WINDOW', self.close_launcher)
+        self.window.mainloop()
 
-window = tk.Tk()
-window.geometry('320x120')
-window.resizable(0,0)
-
-launcher_name = tk.Label(window, text="Super launcher 9000").grid(row=0,column=0)
-
-addr_label = tk.Label(window, text='Ip:').grid(row=1,column=0)
-addr_entr = tk.Entry(window,width=21)
-addr_entr.grid(row=1,column=1)
-
-name_label = tk.Label(window, text='Name:').grid(row=2,column=0)
-name_entr = tk.Entry(window,width=21)
-name_entr.grid(row=2,column=1)
-
-pass_label = tk.Label(window, text='Password:').grid(row=3,column=0)
-password_entr = tk.Entry(window,width=21)
-password_entr.grid(row=3,column=1)
-
-join_button = tk.Button(window, text='Join', command = lambda: join_game(addr_entr.get(), name_entr.get(), password_entr.get())).grid(row=4,column=0)
-
-started = False
-start_button = tk.Button(window, text='Run server', command = lambda: run_serv(serv, started) ).grid(row=4,column=1)
-
-
-def exit_launcher(server):
-    server.running = False
-    window.destroy()
-
-window.protocol('WM_DELETE_WINDOW', lambda: exit_launcher(serv))
-window.mainloop()
-
-
+if __name__ == '__main__':
+    app = App()
+    app.start()
